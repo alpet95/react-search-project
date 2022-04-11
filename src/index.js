a7719d6039ec632139f98d739ae05686498890c7
Alex Petrov <alexpetrov0895@gmail.com>
2022-04-11 12:41:14 +0200

    create: initial file structure.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// ========== components ==========
+import App from "./App";
+// ========== styles ==========
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
